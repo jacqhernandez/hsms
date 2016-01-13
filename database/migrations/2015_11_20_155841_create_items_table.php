aa322fc1298c4810d33da3ce7c9885c4fb4ec4e7
jacqhernandez <jm.hernandez96@yahoo.com>
2016-01-13 16:18:47 +0800

    item to items

--- database/migrations/2015_11_20_155841_create_items_table.php
+++ database/migrations/2015_11_20_155841_create_items_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateItemTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
