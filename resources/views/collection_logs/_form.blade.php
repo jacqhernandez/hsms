b11a8ac467af80268fc6f2108a2f831d86b15763
uselessman63 <mattsgeron@yahoo.com.ph>
2016-01-13 17:22:37 +0800

    Created Collection Log Views

---
+++ resources/views/collection_logs/_form.blade.php
@@ -0,0 +1,71 @@
+<div>
+	<table> 
+		<tbody>
+			<tr>
+				<td> {!! Form::label('name', 'Name: ') !!}</td>
+				<td> {!! Form::text('name', old('name'), ['class' => 'span7']) !!} </td>
+			</tr>	
+			
+			<tr>
+				<td> {!! Form::label('telephone_number', 'Telephone Number: ') !!}</td>
+				<td> {!! Form::text('telephone_number', old('telephone_number'), ['class' => 'span7']) !!} </td>
+			</tr>	
+			
+			<tr>
+				<td> {!! Form::label('address', 'Address: ') !!} </td>
+				<td> {!! Form::text('address', old('address')) !!}</td>
+			</tr>
+			
+			<tr>
+				<td>{!! Form::label('email', 'Email: ') !!}</td>
+				<td>{!! Form::email('email', old('email'), ['class' => 'span7']) !!}</td>
+			</tr>
+			
+			
+			<tr>
+				<td> {!! Form::label('tin', 'TIN: ') !!} </td>
+				<td> {!! Form::input('number', 'tin', old('tin')) !!}</td>
+			</tr>
+
+			<tr>
+				<td>{!! Form::label('contact_person', 'Contact Person: ') !!}</td>
+				<td>{!! Form::input('contact_person', old('contact_person')) !!}</td>
+			</tr>
+			
+			<tr>
+				<td> {!!Form::label('credit_limit', 'Credit Limit: ') !!}</td>
+				<td> {!! Form::input('number','credit_limit', old('credit_limit'), ['class' => 'span3', 'step' => '1']) !!} </td>
+			</tr>
+			
+			<tr>
+				<td> {!! Form::label('status', 'Status: ') !!} </td>
+				<td> {!! Form::select('status', $statusOptions, null) !!}</td>
+			</tr>
+
+			<tr>
+				<td>{!! Form::label('payment_terms', 'Payment Terms: ') !!}</td>
+				<td>{!! Form::select('payment_terms', $paymentOptions, null) !!}</td>
+			</tr>
+
+			<tr>
+				<td>{!! Form::label('username', 'Sales Person: ') !!}</td>
+				<td>{!! Form::select('username', $userOptions, null) !!}</td>
+			</tr>
+			
+		</tbody> 
+	</table>
+	
+	<br>
+	<div class = "submit">
+		{!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+		<a href="{{ action ('ClientsController@index') }}"><button type="button" class="btn btn-info">Back</button></a>
+	</div>
+
+	@if ($errors->any())
+		<ul class="alert alert-danger">
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	@endif
+</div>
