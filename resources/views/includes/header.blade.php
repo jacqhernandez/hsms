14cfde1cbb782769d513df3c056e8cdde5c8abe5
fritzmasiglat <fmasiglat@yahoo.com>
2016-02-18 01:58:02 +0800

    Icons were Added
    Favicons were created in public folder both .ico and .png

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,7 +7,8 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{ url('/home') }}">Hardware Sales Management System</a>
+                <div style="float:left;"><img src="img/Logo.png" style="width:30px; margin-top: 30%; margin-left:30%;"></div>
+                <div style="float:right;"><a class="navbar-brand" href="{{ url('/home') }}">Hardware Sales Management System</a></div>
             </div>
             <!-- /.navbar-header -->
 
